9defe326bda076c5bd2afa60d2b5adb940de035f
Aiapaec64 <denizsemsit@hotmail.be>
2025-11-08 19:28:32 +0100

    Git fix

--- routes/api.php
+++ routes/api.php
@@ -145,6 +145,7 @@ Route::middleware(['web'])->prefix('utilities')->name('utilities.')->group(funct
     Route::post('/log-map-click', LogMapClickController::class)->name('utilities.log_map_click');
 });
 Route::post('/ocr/extract', [OcrController::class, 'extract']);
+
 /* ================== COMPLETIONS ================== */
 Route::prefix('completions')->group(function () {
     Route::get('/', UserCompletionsController::class)->name('api.completions.user');
