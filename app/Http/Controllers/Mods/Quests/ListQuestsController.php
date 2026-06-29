5b067df517ac8113d091a4ffc70b0dc7f6acc71c
Ty <48333668+tylovejoy@users.noreply.github.com>
2026-06-29 09:18:02 -0500

    refactor(moderator): validate list-quests filters, fix import order

--- app/Http/Controllers/Mods/Quests/ListQuestsController.php
+++ app/Http/Controllers/Mods/Quests/ListQuestsController.php
@@ -6,11 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class ListQuestsController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'is_active'  => ['nullable', 'boolean'],
+            'difficulty' => ['nullable', 'string'],
+            'q'          => ['nullable', 'string'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Bad request',
+                'errors'  => $validator->errors(),
+            ], 400);
+        }
+
         $apiRoot   = rtrim((string) config('services.genji_api.root', ''), '/');
         $apiKey    = (string) config('services.genji_api.key', '');
         $sslVerify = (bool) config('services.genji_api.verify', true);
